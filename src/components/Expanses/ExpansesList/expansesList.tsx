3ae58232a021eaaa2471dd8a6dc778914c24f927
mahmoud.hamza <mahmoud.hamza@3ddx.com>
2023-12-07 19:05:09 +0200

    show hint for exceeding the budget

--- src/components/Expanses/ExpansesList/expansesList.tsx
+++ src/components/Expanses/ExpansesList/expansesList.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { ICategory } from '../../../interfaces/Category/ICategory';
 import { Card, Grid } from '@mui/material';
-import {IExpense} from "../../../interfaces/Expanses/IExpanse";
-import {IState} from "../../../interfaces/state/state";
+import { IExpense } from '../../../interfaces/Expanses/IExpanse';
+import { IState } from '../../../interfaces/state/state';
 
 export const ExpenseList: React.FC = () => {
     const categorizedExpenses = useSelector((state: IState) => state.expenses.expenseCategory);
     const categories = useSelector((state: IState) => state.categories.categories);
     const categorizedBudgets = useSelector((state: IState) => state.budget.budgetCategory);
+
+    const calculateTotalExpenses = (expenses: IExpense[]) => {
+        return expenses.reduce((total: number, expense: IExpense) => total + expense.amount, 0);
+    };
 
     return (
         <div className="mt-5 border-bottom">
@@ -16,24 +20,33 @@ export const ExpenseList: React.FC = () => {
             {categories && categories.length > 0 && categories.map((category: ICategory, i: number) => (
                 <div className="border-bottom p-4" key={i}>
                     <div className="d-flex justify-content-start flex-column">
-                    <h5 className="text-secondary">Category: {category.name}</h5>
-                    <p>
-                        Budget: {categorizedBudgets[category.name] ? categorizedBudgets[category.name] : 'Not set'}
-                    </p>
+                        <h5 className="text-secondary">Category: {category.name}</h5>
+                        <p className="mb-0">
+                            Budget: {categorizedBudgets[category.name] ? `$${categorizedBudgets[category.name]}`: 'Not set'}
+                        </p>
+                        {categorizedExpenses[category.name] && categorizedExpenses[category.name].length > 0 && (
+                            <div>
+                                <p>Total Expenses: {`$${calculateTotalExpenses(categorizedExpenses[category.name])}`}</p>
+                                {categorizedBudgets[category.name] &&
+                                calculateTotalExpenses(categorizedExpenses[category.name]) > categorizedBudgets[category.name] && (
+                                    <p className="text-danger">Expenses exceed the budget!</p>
+                                )}
+                            </div>
+                        )}
+                        <Grid container className="border-bottom-1">
+                            {categorizedExpenses[category.name]?.map((expense: IExpense) => (
+                                <Grid item xs={4} key={expense.id}>
+                                    <Card className="p-3" variant="outlined">
+                                        <h6>Expense: {`$${expense.amount}`}</h6>
+                                        <p>Date: {expense.date}</p>
+                                        {expense.notes && (
+                                            <p>Notes: {expense.notes}</p>
+                                        )}
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
                     </div>
-                    <Grid container className="border-bottom-1">
-                        {categorizedExpenses[category.name]?.map((expense: IExpense) => (
-                            <Grid item xs={4} key={expense.id}>
-                                <Card className="p-3" variant="outlined">
-                                    <h6>Expense: {expense.amount}</h6>
-                                    <p>Date: {expense.date}</p>
-                                    { expense.notes &&
-                                        <p>Notes: {expense.notes}</p>
-                                    }
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
                 </div>
             ))}
         </div>
--- src/components/Income/IncomesList.tsx
+++ src/components/Income/IncomesList.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import {IIncome} from "../../interfaces/Income/IIncome";
 import {Card, Grid} from "@mui/material";
+import {IState} from "../../interfaces/state/state";
 
 
 export const IncomesList: React.FC = () => {
-    const incomes = useSelector((state: any) => state.incomes.incomes);
+    const incomes = useSelector((state: IState) => state.incomes.incomes);
     return (
         <div className="mt-2 p-3">
             <h2>Incomes</h2>
@@ -13,8 +14,8 @@ export const IncomesList: React.FC = () => {
                 {incomes.map((income: IIncome) => (
                     <Grid item xs={4} key={income.id}>
                         <Card className="p-3" variant="outlined">
-                            <h4>{income.source}</h4>
-                            <p>{income.amount}</p>
+                            <h4>Source: {income.source}</h4>
+                            <p>{`$${income.amount}`}</p>
                         </Card>
                     </Grid>
                 ))}
